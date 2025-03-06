3216a0d8d667aa7773411f1ea742c1adb1f34764
Icecold334 <fauzanimam334@gmail.com>
2025-03-06 13:24:34 +0700

    first commit

---
+++ resources/js/pages/dashboard.tsx
@@ -0,0 +1,20 @@
+import { Switch } from '@/components/ui/switch';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+];
+
+export default function Dashboard() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Dashboard" />
+            <Switch />
+        </AppLayout>
+    );
+}
